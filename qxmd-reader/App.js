f635b0c8ea9688d32c2fbf4a03a1c5aecc2cd20e
oliverpople <oliverpople@gmail.com>
2019-03-19 11:50:15 -0700

    App.js now request News data and renders articles in a flatlist.

--- qxmd-reader/App.js
+++ qxmd-reader/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { FlatList } from "react-native";
+import { getNews } from "./src/news";
+import Article from "./src/components/Article";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { articles: [], refreshing: true };
+    this.fetchNews = this.fetchNews.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchNews();
+  }
+
+  fetchNews() {
+    getNews()
+      .then(articles => this.setState({ articles, refreshing: false }))
+      .catch(() => this.setState({ refreshing: false }));
+  }
+
+  handleRefresh() {
+    this.setState(
+      {
+        refreshing: true
+      },
+      () => this.fetchNews()
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <FlatList
+        data={this.state.articles}
+        renderItem={({ item }) => <Article article={item} />}
+        keyExtractor={item => item.url}
+        refreshing={this.state.refreshing}
+        onRefresh={this.handleRefresh.bind(this)}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
